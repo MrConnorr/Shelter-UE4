e30e935a9e834a536d7149a1cc2476033d889c35
Roman Konstantinov <dragonden2010@gmail.com>
2022-02-22 14:21:27 +0200

    Initial commit
    - Uploaded full shelter project
    - Translated main text to English

---
+++ README.md
@@ -0,0 +1,3 @@
+# Shelter
+
+Developed with Unreal Engine 4
---
+++ Source/Shelter/ShelterGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShelterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHELTER_API AShelterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
